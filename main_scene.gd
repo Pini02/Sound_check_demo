87e902a723637794958156c9d8827b66fadefcf8
Pini02 <marcopini87@gmail.com>
2024-03-05 18:51:15 +0100

    aggiunta demo con collision shape in cui il suono è ridotto

---
+++ main_scene.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AudioStreamPlayer.playing=true
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if $AudioStreamPlayer.is_playing() == true and $ZeroSound.overlaps_body($RigidBody2D)==true:
+		print("SAFE")
+		$AudioStreamPlayer.set_volume_db(-10.0)
+	elif $AudioStreamPlayer.is_playing() == true and $ZeroSound.overlaps_body($RigidBody2D)==false:
+		print("NOT SAFE")
+	else:
+		print("No sound")

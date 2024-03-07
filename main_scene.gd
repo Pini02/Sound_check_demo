3183088a595b9f593ed5147ebb07673c4354771e
Pini02 <marcopini87@gmail.com>
2024-03-07 16:06:21 +0100

    scene oggetto e personaggio(muovibile) aggiornamento main scene

--- main_scene.gd
+++ main_scene.gd
@@ -9,10 +9,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $AudioStreamPlayer.is_playing() == true and $ZeroSound.overlaps_body($RigidBody2D)==true:
-		print("SAFE new commit")
+	if $AudioStreamPlayer.is_playing() == true and $ZeroSound.overlaps_body($Character)==true:
+		print("SAFE")
 		$AudioStreamPlayer.set_volume_db(-10.0)
-	elif $AudioStreamPlayer.is_playing() == true and $ZeroSound.overlaps_body($RigidBody2D)==false:
+	elif $AudioStreamPlayer.is_playing() == true and $ZeroSound.overlaps_body($Character)==false:
 		print("NOT SAFE")
 	else:
 		print("No sound")
+
+
+func _on_key_body_entered(body):
+	$Character.add_inventory("Key")
+	print("chiave")
+	$Key.queue_free()
